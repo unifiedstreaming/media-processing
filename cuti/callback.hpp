3d6a2decdeb1e3e5de4a2a72138dfd6df47d582d
Wil Evers <wil@unified-streaming.com>
2021-04-19 11:57:13 +0000

    Introducing callback_t as a std::function<void()> alternative

---
+++ cuti/callback.hpp
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CUTI_CALLBACK_HPP_
+#define CUTI_CALLBACK_HPP_
+
+#include "linkage.h"
+
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace cuti
+{
+
+/*
+ * Type-erased callback wrapper as a workaround for some of the
+ * problems of std::function<void()>.
+ */
+struct CUTI_ABI callback_t
+{
+  callback_t() noexcept
+  : impl_(nullptr)
+  { }
+
+  callback_t(std::nullptr_t) noexcept
+  : impl_(nullptr)
+  { }
+
+  template<typename F, typename = std::enable_if_t<
+    !std::is_convertible_v<std::decay_t<F>*, callback_t const*>>>
+  callback_t(F&& f)
+  : impl_(std::make_shared<impl_instance_t<std::decay_t<F>>>(
+      std::forward<F>(f)))
+  { }
+
+  callback_t(callback_t const& rhs) noexcept
+  : impl_(rhs.impl_)
+  { }
+
+  callback_t(callback_t&& rhs) noexcept
+  : impl_(std::move(rhs.impl_))
+  { }
+
+  callback_t& operator=(std::nullptr_t) noexcept
+  {
+    impl_ = nullptr;
+    return *this;
+  }
+
+  template<typename F, typename = std::enable_if_t<
+    !std::is_convertible_v<std::decay_t<F>*, callback_t const*>>>
+  callback_t& operator=(F&& f)
+  {
+    impl_ = std::make_shared<impl_instance_t<std::decay_t<F>>>(
+      std::forward<F>(f));
+    return *this;
+  }
+
+  callback_t& operator=(callback_t const& rhs) noexcept
+  {
+    impl_ = rhs.impl_;
+    return *this;
+  }
+
+  callback_t& operator=(callback_t&& rhs) noexcept
+  {
+    impl_ = std::move(rhs.impl_);
+    return *this;
+  }
+
+  explicit operator bool() const noexcept
+  {
+    return impl_ != nullptr;
+  }
+
+  void swap(callback_t& other) noexcept
+  {
+    impl_.swap(other.impl_);
+  }
+
+  void operator()() const
+  {
+    assert(*this != nullptr);
+    (*impl_)();
+  }
+
+  friend bool operator==(callback_t const& lhs, std::nullptr_t) noexcept
+  {
+    return !lhs.operator bool();
+  }
+
+  friend bool operator==(std::nullptr_t, callback_t const& rhs) noexcept
+  {
+    return !rhs.operator bool();
+  }
+
+  friend bool operator!=(callback_t const& lhs, std::nullptr_t) noexcept
+  {
+    return lhs.operator bool();
+  }
+
+  friend bool operator!=(std::nullptr_t, callback_t const& rhs) noexcept
+  {
+    return rhs.operator bool();
+  }
+
+private :
+  struct impl_t
+  {
+    impl_t()
+    { }
+
+    impl_t(impl_t const&) = delete;
+    impl_t& operator=(impl_t const&) = delete;
+
+    virtual void operator()() const = 0;
+
+    virtual ~impl_t()
+    { }
+  };
+
+  template<typename F>
+  struct impl_instance_t : impl_t
+  {
+    impl_instance_t(F const& f)
+    : impl_t()
+    , f_(f)
+    { }
+
+    impl_instance_t(F&& f)
+    : impl_t()
+    , f_(std::move(f))
+    { }
+
+    void operator()() const override
+    {
+      f_();
+    }
+
+  private :
+    F f_;
+  };
+
+private :
+  std::shared_ptr<impl_t const> impl_;
+};
+
+CUTI_ABI
+inline void swap(callback_t& cb1, callback_t& cb2) noexcept
+{
+  cb1.swap(cb2);
+}
+
+} // cuti
+
+#endif
---
+++ cuti/unit_tests/callback_test.cpp
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "callback.hpp"
+
+#include <utility>
+#include <type_traits>
+
+// Enable assert
+#undef NDEBUG
+#include <cassert>
+
+namespace // anonymous
+{
+
+using namespace cuti;
+
+struct functor_t
+{
+  functor_t(bool& called)
+  : called_(called)
+  { }
+
+  void operator()() const
+  {
+    called_ = true;
+  }
+
+private :
+  bool& called_;
+};
+
+static_assert(std::is_nothrow_default_constructible_v<callback_t>);
+static_assert(std::is_nothrow_copy_constructible_v<callback_t>);
+static_assert(std::is_nothrow_move_constructible_v<callback_t>);
+static_assert(std::is_nothrow_copy_assignable_v<callback_t>);
+static_assert(std::is_nothrow_move_assignable_v<callback_t>);
+static_assert(std::is_nothrow_swappable_v<callback_t>);
+
+// check that callback's templated constructor and assigment operator
+// (which allocate an impl object) are not noexcept
+static_assert(!std::is_nothrow_constructible_v<callback_t, functor_t const&>);
+static_assert(!std::is_nothrow_assignable_v<callback_t, functor_t const&>);
+static_assert(!std::is_nothrow_constructible_v<callback_t, functor_t&>);
+static_assert(!std::is_nothrow_assignable_v<callback_t, functor_t&>);
+static_assert(!std::is_nothrow_constructible_v<callback_t, functor_t&&>);
+static_assert(!std::is_nothrow_assignable_v<callback_t, functor_t&&>);
+
+// check that callback_t's templated constructor and assignment operator
+// are properly SFINEA'd out for callback_t lvalues
+static_assert(std::is_nothrow_constructible_v<callback_t, callback_t&>);
+static_assert(std::is_nothrow_assignable_v<callback_t, callback_t&>);
+
+bool function_called = false;
+
+void function()
+{
+  function_called = true;
+}
+
+void empty_callback()
+{
+  callback_t cb;
+  assert(cb == nullptr);
+}
+
+void function_callback()
+{
+  callback_t cb(function);
+  assert(cb != nullptr);
+
+  function_called = false;
+  cb();
+  assert(function_called);
+}
+
+void functor_callback()
+{
+  bool called = false;
+  callback_t cb = functor_t(called);
+  assert(cb != nullptr);
+  cb();
+  assert(called);
+}
+
+void lambda_callback()
+{
+  bool called = false;
+  callback_t cb([&] { called = true; });
+  assert(cb != nullptr);
+  cb();
+  assert(called);
+}
+
+void copy_construct()
+{
+  callback_t cb1(function);
+  assert(cb1 != nullptr);
+
+  callback_t cb2(cb1);
+  assert(cb1 != nullptr);
+  assert(cb2 != nullptr);
+}
+  
+void move_construct()
+{
+  callback_t cb1(function);
+  assert(cb1 != nullptr);
+
+  callback_t cb2(std::move(cb1));
+  assert(cb1 == nullptr);
+  assert(cb2 != nullptr);
+}
+  
+void copy_assign()
+{
+  callback_t cb1(function);
+  assert(cb1 != nullptr);
+
+  callback_t cb2;
+  cb2 = cb1;
+  assert(cb1 != nullptr);
+  assert(cb2 != nullptr);
+}
+  
+void move_assign()
+{
+  callback_t cb1(function);
+  assert(cb1 != nullptr);
+
+  callback_t cb2;
+  cb2 = std::move(cb1);
+  assert(cb1 == nullptr);
+  assert(cb2 != nullptr);
+}
+
+void swapped()
+{
+  callback_t cb1(function);
+  assert(cb1 != nullptr);
+
+  callback_t cb2;
+  assert(cb2 == nullptr);
+
+  swap(cb1, cb2);
+  assert(cb1 == nullptr);
+  assert(cb2 != nullptr);
+}
+  
+} // anonymous
+
+int main()
+{
+  empty_callback();
+  function_callback();
+  functor_callback();
+  lambda_callback();
+
+  copy_construct();
+  move_construct();
+  copy_assign();
+  move_assign();
+  swapped();
+
+  return 0;
+}
+
