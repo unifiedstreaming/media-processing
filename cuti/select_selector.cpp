134eccfd9ff8197e4ec7b5bc3891d4fb726e8978
Wil Evers <wil@unified-streaming.com>
2021-05-01 17:25:13 +0000

    One more glitch fix...

--- cuti/select_selector.cpp
+++ cuti/select_selector.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of version 2.1 of the GNU Lesser
  * General Public License along with the cuti library.  If not, see
  * <http://www.gnu.org/licenses/>.
- */es
+ */
  
 
 #include "select_selector.hpp"
