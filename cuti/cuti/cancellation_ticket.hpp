d8695f813d98df8bb1c3083ffd42680423975a63
Wil Evers <wil@unified-streaming.com>
2021-07-07 08:33:46 +0000

    Give cancellation_ticket its own header

---
+++ cuti/cuti/cancellation_ticket.hpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CUTI_CANCELLATION_TICKET_HPP_
+#define CUTI_CANCELLATION_TICKET_HPP_
+
+#include "linkage.h"
+
+namespace cuti
+{
+
+struct scheduler_t;
+
+/*
+ * Cancellation tickets are used to cancel a previously scheduled
+ * scheduler callback before it is invoked.  See scheduler.hpp for
+ * details.
+ * 
+ * Please note: a cancellation ticket is only valid until the callback
+ * is selected.  Any attempt to cancel a callback during or after its
+ * invocation leads to undefined behavior.
+ */
+struct CUTI_ABI cancellation_ticket_t
+{
+  /*
+   * Constructs an empty cancellation ticket.
+   */
+  cancellation_ticket_t() noexcept
+  : type_(type_t::empty)
+  , id_(-1)
+  { }
+
+  /*
+   * Tells if the ticket is empty.  Scheduling a callback returns a
+   * non-empty cancellation ticket.
+   */
+  bool empty() const noexcept
+  { return type_ == type_t::empty; }
+
+  /*
+   * Sets the ticket to the empty state.
+   */
+  void clear() noexcept
+  { *this = cancellation_ticket_t(); }
+
+private :
+  friend struct scheduler_t;
+
+  enum class type_t { empty, alarm, writable, readable };
+
+  explicit cancellation_ticket_t(type_t type, int id) noexcept
+  : type_(type)
+  , id_(id)
+  { }
+
+  type_t type() const noexcept
+  { return type_; }
+
+  int id() const noexcept
+  { return id_; }
+
+private :
+  type_t type_;
+  int id_;
+};
+
+} // cuti
+
+#endif
--- cuti/cuti/scheduler.hpp
+++ cuti/cuti/scheduler.hpp
@@ -21,6 +21,7 @@
 #define CUTI_SCHEDULER_HPP_
 
 #include "callback.hpp"
+#include "cancellation_ticket.hpp"
 #include "chrono_types.hpp"
 #include "linkage.h"
 
@@ -30,60 +31,6 @@
 
 namespace cuti
 {
-
-struct scheduler_t;
-
-/*
- * Cancellation tickets are used to cancel a previously scheduled
- * callback before it is invoked.
- * 
- * Please note: a cancellation ticket is only valid until the callback
- * is selected.  Any attempt to cancel a callback during or after its
- * invocation leads to undefined behavior.
- */
-struct cancellation_ticket_t
-{
-  /*
-   * Constructs an empty cancellation ticket.
-   */
-  cancellation_ticket_t() noexcept
-  : type_(type_t::empty)
-  , id_(-1)
-  { }
-
-  /*
-   * Tells if the ticket is empty.  Scheduling a callback returns a
-   * non-empty cancellation ticket.
-   */
-  bool empty() const noexcept
-  { return type_ == type_t::empty; }
-
-  /*
-   * Sets the ticket to the empty state.
-   */
-  void clear() noexcept
-  { *this = cancellation_ticket_t(); }
-
-private :
-  friend struct scheduler_t;
-
-  enum class type_t { empty, alarm, writable, readable };
-
-  explicit cancellation_ticket_t(type_t type, int id) noexcept
-  : type_(type)
-  , id_(id)
-  { }
-
-  type_t type() const noexcept
-  { return type_; }
-
-  int id() const noexcept
-  { return id_; }
-
-private :
-  type_t type_;
-  int id_;
-};
 
 /*
  * Abstract event scheduler interface.  The purpose of this interface
