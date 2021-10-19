b72fc264165afc78f75d691902f2e8ac65fb6026
Wil Evers <wil@unified-streaming.com>
2021-10-19 12:39:01 +0000

    Added throughput_checker

---
+++ cuti/cuti/throughput_tracker.hpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CUTI_THROUGHPUT_TRACKER_HPP_
+#define CUTI_THROUGHPUT_TRACKER_HPP_
+
+#include "chrono_types.hpp"
+#include "linkage.h"
+
+#include <cstddef>
+
+namespace cuti
+{
+
+struct CUTI_ABI throughput_tracker_t
+{
+  /*
+   * Constructs a throughput tracker.  The throughput is considered to
+   * be low if less than min_bytes_per_tick were transferred for at
+   * least slow_ticks_limit ticks.
+   */
+  throughput_tracker_t(std::size_t min_bytes_per_tick,
+                       unsigned int slow_ticks_limit,
+                       duration_t tick_length = seconds_t(1));
+
+  /*
+   * Reports the time of the next tick, which is a good moment to check
+   * for slowness.
+   */
+  time_point_t next_tick() const
+  {
+    return next_tick_;
+  }
+
+  /*
+   * Records a data transfer.  If the next tick is less than or equal to
+   * the current time, it is advanced.
+   */
+  void record_transfer(std::size_t n_bytes);
+
+  /*
+   * Tells if the throughput is low.  If the next tick is less than or
+   * equal to the current time, it is advanced.
+   */
+  bool is_low();
+
+private :
+  void update();
+
+private :
+  std::size_t min_bytes_per_tick_;
+  unsigned int slow_ticks_limit_;
+  duration_t tick_length_;
+  time_point_t next_tick_;
+  std::size_t current_tick_bytes_;
+  unsigned int n_slow_ticks_;
+};
+
+} // cuti
+
+#endif
---
+++ cuti/unit_tests/throughput_tracker_test.cpp
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <cuti/throughput_tracker.hpp>
+
+#include <thread>
+
+#undef NDEBUG
+#include <cassert>
+
+namespace // anonymous
+{
+
+using namespace cuti;
+
+void await(time_point_t until)
+{
+  auto now = cuti_clock_t::now();
+  while(now < until)
+  {
+    std::this_thread::sleep_for(until - now);
+    now = cuti_clock_t::now();
+  }
+}
+    
+void test_next_tick()
+{
+  /*
+   * verify that constructing a tracker, recording a transfer and
+   * checking for low speed each set the next tick to somewhere in the
+   * future
+   */
+  auto now = cuti_clock_t::now();
+  throughput_tracker_t tracker(1, 1, milliseconds_t(1));
+  auto next = tracker.next_tick();
+  assert(next > now);
+
+  await(next);
+  now = next;
+
+  tracker.record_transfer(0);
+  next = tracker.next_tick();
+  assert(next > now);
+
+  await(next);
+  now = next;
+
+  tracker.is_low();
+  next = tracker.next_tick();
+  assert(next > now);
+}
+  
+void test_low_speed()
+{
+  {
+    throughput_tracker_t tracker(512, 1, milliseconds_t(1));
+
+    await(tracker.next_tick());
+    assert(tracker.is_low());
+  }
+
+  {
+    throughput_tracker_t tracker(512, 2, milliseconds_t(1));
+
+    await(tracker.next_tick());
+    tracker.is_low(); // could be false (unlikely), but advances next_tick
+
+    await(tracker.next_tick());
+    assert(tracker.is_low()); // must be true after second tick
+  }
+
+  {
+    throughput_tracker_t tracker(512, 1, milliseconds_t(1));
+    tracker.record_transfer(0);
+
+    await(tracker.next_tick());
+    assert(tracker.is_low());
+  }
+
+  {
+    throughput_tracker_t tracker(512, 2, milliseconds_t(1));
+    tracker.record_transfer(0);
+
+    await(tracker.next_tick());
+    tracker.record_transfer(0); // advances next_tick
+
+    await(tracker.next_tick());
+    assert(tracker.is_low()); // must be true after second tick
+  }
+
+  {
+    throughput_tracker_t tracker(512, 1, milliseconds_t(1));
+    tracker.record_transfer(511);
+
+    await(tracker.next_tick());
+    assert(tracker.is_low());
+  }
+
+  {
+    throughput_tracker_t tracker(512, 2, milliseconds_t(1));
+    tracker.record_transfer(511);
+
+    await(tracker.next_tick());
+    tracker.record_transfer(511); // advances next_tick
+
+    await(tracker.next_tick());
+    assert(tracker.is_low()); // must be true after second tick
+  }
+
+  {
+    throughput_tracker_t tracker(512, 1, milliseconds_t(1));
+    tracker.record_transfer(512);
+
+    await(tracker.next_tick());
+    tracker.record_transfer(511); // advances next_tick
+
+    await(tracker.next_tick());
+    assert(tracker.is_low()); // must be true after second tick
+  }
+}
+
+void test_sufficient_speed()
+{
+  throughput_tracker_t tracker(512, 100000, milliseconds_t(1));
+  tracker.record_transfer(512);
+
+  assert(!tracker.is_low()); // only low if 100 seconds have passed
+}
+
+} // anonymous
+
+int main()
+{
+  test_next_tick();
+  test_low_speed();
+  test_sufficient_speed();
+
+  return 0;
+}
