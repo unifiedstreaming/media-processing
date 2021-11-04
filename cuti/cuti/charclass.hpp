d784280cc7039971a32699b41e6714bd6510451f
Wil Evers <wil@unified-streaming.com>
2021-11-04 07:53:50 +0000

    Write hex escapes in upper case

--- cuti/cuti/charclass.hpp
+++ cuti/cuti/charclass.hpp
@@ -58,7 +58,7 @@ inline int hex_digit_value(int c)
 
 char constexpr hex_digits[] = {
   '0', '1', '2', '3', '4', '5', '6', '7',
-  '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 };
 
 } // cuti
