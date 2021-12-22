0099e1f5e79ef862d6bee4f44c3e351f73855aa9
Wil Evers <wil@unified-streaming.com>
2021-12-22 09:48:52 +0000

    Added sequence_of

---
+++ cuti/cuti/sequence_of.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 CodeShop B.V.
+ *
+ * This file is part of the cuti library.
+ *
+ * The cuti library is free software: you can redistribute it and/or
+ * modify it under the terms of version 2.1 of the GNU Lesser General
+ * Public License as published by the Free Software Foundation.
+ *
+ * The cuti library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See version
+ * 2.1 of the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the cuti library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CUTI_SEQUENCE_OF_HPP_
+#define CUTI_SEQUENCE_OF_HPP_
+
+namespace cuti
+{
+
+/*
+ * Tag type for denoting a sequence of values.
+ */
+template<typename T>
+struct sequence_of_t
+{ };
+
+} // cuti
+
+#endif
