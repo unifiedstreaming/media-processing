7455d9ac50cce3d8df1df433a879209915b507fa
Wil Evers <wil@unified-streaming.com>
2022-03-22 10:49:47 +0000

    Imported libx264 (copied from http://svn-externals.internal.unified-streaming.com/svn/trunk/h264-lib/x264-0.164.3079+gitd9a19f0)

---
+++ libx264/version.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script modified from upstream source for Debian packaging since packaging
+# won't include .git repository.
+echo '#define X264_REV 3079'
+echo '#define X264_REV_DIFF 0'
+echo '#define X264_VERSION " r3079 d9a19f0"'
+echo '#define X264_POINTVER "0.164.3079 d9a19f0"'
