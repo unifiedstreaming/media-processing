40143d2f871239446d5330c61e4f2f02504b1098
Wil Evers <wil@unified-streaming.com>
2023-05-03 13:18:59 +0000

    Fix licensing notices x264_proto unit tests (GPL -> LGPL)

--- x264_proto/unit_tests/proto_test.cpp
+++ x264_proto/unit_tests/proto_test.cpp
@@ -1,20 +1,22 @@
 /*
  * Copyright (C) 2023 CodeShop B.V.
  *
- * This file is part of the x264_proto library.
+ * This file is part of the x264 service protocol library.
  *
- * The x264_proto library is free software: you can redistribute it
- * and/or modify it under the terms of version 2 of the GNU General
- * Public License as published by the Free Software Foundation.
+ * The x264 service protocol library is free software: you can
+ * redistribute it and/or modify it under the terms of version 2.1 of
+ * the GNU Lesser General Public License as published by the Free
+ * Software Foundation.
  *
- * The x264_proto library is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See version 2 of the GNU General Public License for more details.
- *
- * You should have received a copy of version 2 of the GNU General
- * Public License along with the x264_proto library.  If not, see
- * <http://www.gnu.org/licenses/>.
+ * The x264 service protocol library is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See version 2.1 of the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of version 2.1 of the GNU Lesser
+ * General Public License along with the x264 service protocol
+ * library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <cuti/cmdline_reader.hpp>
